8ee99bc8e99e52340d3f31aeea65adcfafef3bd5
thaong197 <47881831+thaong197@users.noreply.github.com>
2019-08-10 19:39:25 -0700

    Add files via upload

---
+++ send_sms.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'ACef98e9bafdcd986c65d3aa7a02ad1898'
+auth_token = 'afb029370cd6e2b9f2dffb3f40e62207'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                  .create(
+                  body = 'hello world', from_ = '+14157410967', to = "+14152158296")
+
+print(message.sid)
